a38582e27b05ac37793539990a9e30a64989149c
Gleydson Costa <costalince@gmail.com>
2014-11-30 12:33:04 -0200

    Connect database

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,24 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+  protected function _initConnection() {
+    $options = $this->getOption('resources');
 
+    $db_adapter = $options['db']['adapter'];
+    $params = $options['db']['params'];
+
+    try {
+
+      $db = Zend_Db :: factory($db_adapter,$params);
+
+      $db->getConnection();
+
+      $registry = Zend_Registry :: getInstance();
+      $registry->set('db', $db);
+
+    } catch (Zend_Exception $e) {
+      echo "Estamos com problemas em nossos servidores. Favor aguarde!";
+    }
+
+  }
 }
-
